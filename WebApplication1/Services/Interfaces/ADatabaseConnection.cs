5b55074ad769f8f70d1bcd01d759301432c6ba0b
artemqdh <artjom-05@mail.ru>
2025-04-27 12:00:03 +0300

    Added all files. Closed #1

---
+++ WebApplication1/Services/Interfaces/ADatabaseConnection.cs
@@ -0,0 +1,20 @@
+﻿using WebApplication1.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Services.Interfaces
+{
+    public abstract class ADatabaseConnection : DbContext
+    {
+        protected abstract string ReturnConnectionString();
+
+        protected string ConnectionString { get; private set; }
+
+        public DbSet<Gym> Gyms => Set<Gym>();
+
+        public ADatabaseConnection()
+        {
+            this.ConnectionString = this.ReturnConnectionString();
+            this.Database.EnsureCreated();
+        }
+    }
+}
