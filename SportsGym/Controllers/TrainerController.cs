13e45a327166524725e49bd015fb7ed4d91cec25
artemqdh <161634897+artemqdh@users.noreply.github.com>
2025-05-03 14:48:15 +0300

    Added controllers. Closed #17 (#18)

---
+++ SportsGym/Controllers/TrainerController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SportsGym.Models.Entities;
+using SportsGym.Services;
+using System.ComponentModel;
+
+namespace SportsGym.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrainerController : ControllerBase
+    {
+        private readonly PostgresConnection _db;
+
+        public TrainerController(PostgresConnection db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<List<Trainer>> GetTrainers() ///< Get all trainers
+        {
+            return await _db.Trainers.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Trainer>> GetTrainer(int id) ///< Get trainer by id
+        {
+            Trainer trainer = await _db.Trainers.FindAsync(id);
+            if (trainer == null)
+            {
+                return NotFound();
+            }
+
+            return trainer;
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Manager,Admin")]
+        public async Task<ActionResult<Trainer>> PostTrainer(Trainer trainer) ///< Add new trainer
+        {
+            _db.Trainers.Add(trainer);
+            await _db.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetTrainer), new { id = trainer.Id }, trainer);
+        }
+
+        
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "Manager,Admin")]
+        public async Task<IActionResult> DeleteTrainer(int id) ///< Delete a trainer
+        {
+            var trainer = await _db.Trainers.FindAsync(id);
+            if (trainer == null)
+            {
+                return NotFound();
+            }
+            _db.Trainers.Remove(trainer);
+            await _db.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ SportsGym/Controllers/TrainingScheduleController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SportsGym.Models.Entities;
+using SportsGym.Services;
+
+namespace SportsGym.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrainingScheduleController : ControllerBase
+    {
+        private readonly PostgresConnection _db;
+
+        public TrainingScheduleController(PostgresConnection db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<List<TrainingSchedule>> GetSchedules() ///< Get all training schedules
+        {
+            return await _db.Trainings.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TrainingSchedule>> GetSchedule(int id) ///< Get schedule by id
+        {
+            var schedule = await _db.Trainings.FindAsync(id);
+            if (schedule == null)
+                return NotFound();
+            return schedule;
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Manager,Admin")]
+        public async Task<ActionResult<TrainingSchedule>> PostSchedule(TrainingSchedule schedule) ///< Add new schedule
+        {
+            _db.Trainings.Add(schedule);
+            await _db.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetSchedule), new { id = schedule.Id }, schedule);
+        }
+
+        [HttpPut("{id}")]
+        [Authorize(Roles = "Manager,Admin")]
+        public async Task<IActionResult> PutSchedule(int id, TrainingSchedule updatedSchedule) ///< Update an existing schedule
+        {
+            if (id != updatedSchedule.Id)
+                return BadRequest();
+
+            _db.Entry(updatedSchedule).State = EntityState.Modified;
+            await _db.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "Manager,Admin")]
+        public async Task<IActionResult> DeleteSchedule(int id) ///< Delete a schedule
+        {
+            var schedule = await _db.Trainings.FindAsync(id);
+            if (schedule == null)
+                return NotFound();
+
+            _db.Trainings.Remove(schedule);
+            await _db.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
